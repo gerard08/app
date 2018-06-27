e2d3d4d7eff20cbf5c510864f17ea6bccb0e4eec
unknown <gerardmaresp@gmail.com>
2018-06-28 00:24:55 +0200

    first commit

--- app/src/main/java/com/mars/bookme/logIn.java
+++ app/src/main/java/com/mars/bookme/logIn.java
@@ -1,19 +1,41 @@
 package com.mars.bookme;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.Manifest.permission.INTERNET;
 
 public class logIn extends AppCompatActivity {
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.loginlayout);
         LinearLayout your_Layout = findViewById(R.id.main_container);
         AnimationDrawable animationDrawable = (AnimationDrawable) your_Layout.getBackground();
@@ -21,4 +43,33 @@ public class logIn extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
     }
+public void login(){
+        post();
+}
+   private void post(){
+       final TextView mTextView = (TextView) findViewById(R.id.texto);
+// ...
+
+// Instantiate the RequestQueue.
+       RequestQueue queue = Volley.newRequestQueue(this);
+       String url ="79.151.158.17";
+
+// Request a string response from the provided URL.
+       StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+               new Response.Listener<String>() {
+                   @Override
+                   public void onResponse(String response) {
+                       // Display the first 500 characters of the response string.
+                       mTextView.setText("Response is: "+ response.substring(0,500));
+                   }
+               }, new Response.ErrorListener() {
+           @Override
+           public void onErrorResponse(VolleyError error) {
+               mTextView.setText("That didn't work!");
+           }
+       });
+
+// Add the request to the RequestQueue.
+       queue.add(stringRequest);
+   }
 }
