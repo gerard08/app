7c2a25ad55a84aae960420532305e0fb034ae0ab
unknown <gerardmaresp@gmail.com>
2018-06-29 01:27:33 +0200

    first commit

--- app/src/main/java/com/mars/bookme/logIn.java
+++ app/src/main/java/com/mars/bookme/logIn.java
@@ -1,18 +1,15 @@
 package com.mars.bookme;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.support.v4.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,11 +17,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import static android.Manifest.permission.INTERNET;
 
 public class logIn extends AppCompatActivity {
     private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
@@ -43,33 +35,40 @@ public class logIn extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
     }
-public void login(){
-        post();
-}
-   private void post(){
-       final TextView mTextView = (TextView) findViewById(R.id.texto);
-// ...
 
+    public void login(View view) {
+        get();
+    }
+
+    private void get() {
+// ...
 // Instantiate the RequestQueue.
-       RequestQueue queue = Volley.newRequestQueue(this);
-       String url ="79.151.158.17";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://86ed368e.ngrok.io/";
 
 // Request a string response from the provided URL.
-       StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-               new Response.Listener<String>() {
-                   @Override
-                   public void onResponse(String response) {
-                       // Display the first 500 characters of the response string.
-                       mTextView.setText("Response is: "+ response.substring(0,500));
-                   }
-               }, new Response.ErrorListener() {
-           @Override
-           public void onErrorResponse(VolleyError error) {
-               mTextView.setText("That didn't work!");
-           }
-       });
+        StringRequest stringRequest = new StringRequest(Request.Method.DEPRECATED_GET_OR_POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Succesfully connected!");
+                        Toast.makeText(logIn.this, "Response is: " + response, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(logIn.this, "That didn't work!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 // Add the request to the RequestQueue.
-       queue.add(stringRequest);
-   }
+        queue.add(stringRequest);
+
+
+    }
+
+    private void post() {
+
+    }
 }
