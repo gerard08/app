c7596312458e56d4cf9673e24789147a2230e964
unknown <gerardmaresp@gmail.com>
2018-07-01 17:51:59 +0200

    first commit

--- app/src/main/java/com/mars/bookme/logIn.java
+++ app/src/main/java/com/mars/bookme/logIn.java
@@ -1,22 +1,29 @@
 package com.mars.bookme;
 
 import android.Manifest;
+import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class logIn extends AppCompatActivity {
     private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
@@ -39,12 +46,13 @@ public class logIn extends AppCompatActivity {
     public void login(View view) {
         get();
     }
+    public void register(View view){post(this);}
 
     private void get() {
 // ...
 // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://86ed368e.ngrok.io/";
+        String url = "https://8199be71.ngrok.io";
 
 // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.DEPRECATED_GET_OR_POST, url,
@@ -68,7 +76,43 @@ public class logIn extends AppCompatActivity {
 
     }
 
-    private void post() {
 
+
+    private void post(Context context){
+        final EditText user = findViewById(R.id.email);
+        final EditText passwd =findViewById(R.id.passwd);
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST,"https://8199be71.ngrok.io", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("user",user.getText().toString());
+                params.put("pass",passwd.getText().toString());
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        queue.add(sr);
+    }
+
+    public interface PostCommentResponseListener {
+        public void requestStarted();
+        public void requestCompleted();
+        public void requestEndedWithError(VolleyError error);
     }
 }
