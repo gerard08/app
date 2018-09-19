06a62c5e83d500851c42ea81e324ebbc0889f86d
unknown <gerardmaresp@gmail.com>
2018-09-20 00:33:56 +0200

    new

--- app/src/main/java/com/mars/bookme/logIn.java
+++ app/src/main/java/com/mars/bookme/logIn.java
@@ -2,6 +2,8 @@ package com.mars.bookme;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,7 +15,6 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,11 +30,16 @@ public class logIn extends AppCompatActivity {
     private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
     private static final String[] REQUIRED_SDK_PERMISSIONS = new String[]{
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
+    private String pass;
+    private boolean back;
+    private String email;
+    private String password;
+    private Boolean New;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        check();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.loginlayout);
         LinearLayout your_Layout = findViewById(R.id.main_container);
@@ -41,63 +47,46 @@ public class logIn extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(4000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
-    }
 
-    public void login(View view) {
+    }
+    public void loginclick(View view){
+        login();
+    }
+    public void login() {
+        post(this);
         get();
     }
-    public void register(View view){post(this);}
-
-    private void get() {
-// ...
-// Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://8199be71.ngrok.io";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.DEPRECATED_GET_OR_POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        //mTextView.setText("Succesfully connected!");
-                        Toast.makeText(logIn.this, "Response is: " + response, Toast.LENGTH_SHORT).show();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(logIn.this, "That didn't work!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
-
-    }
-
-
+    public void register(View view){
+        Intent i = new Intent(getBaseContext(), register.class);
+        startActivity(i);}
 
     private void post(Context context){
-        final EditText user = findViewById(R.id.email);
-        final EditText passwd =findViewById(R.id.passwd);
+        if(back==true){
+            final EditText mail = findViewById(R.id.email);
+            final EditText passwd =findViewById(R.id.passwd);
+            email = mail.getText().toString();
+            password = passwd.getText().toString();
+            Toast.makeText(context, "variables", Toast.LENGTH_SHORT).show();
+        }
         RequestQueue queue = Volley.newRequestQueue(context);
-        StringRequest sr = new StringRequest(Request.Method.POST,"https://8199be71.ngrok.io", new Response.Listener<String>() {
+        StringRequest sr = new StringRequest(Request.Method.POST,"http://marsdevelopers.ddns.net/", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                Toast.makeText(logIn.this, "error", Toast.LENGTH_SHORT).show();
             }
         }){
             @Override
             protected Map<String,String> getParams(){
                 Map<String,String> params = new HashMap<String, String>();
-                params.put("user",user.getText().toString());
-                params.put("pass",passwd.getText().toString());
-
+                params.put("page","login");
+                params.put("mail",email);
+                params.put("pass",password);
                 return params;
+
             }
 
             @Override
@@ -109,10 +98,71 @@ public class logIn extends AppCompatActivity {
         };
         queue.add(sr);
     }
+    private void get (){
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+// ...
 
-    public interface PostCommentResponseListener {
-        public void requestStarted();
-        public void requestCompleted();
-        public void requestEndedWithError(VolleyError error);
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://marsdevelopers.ddns.net/";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        pass = response.substring(0);
+
+                        if(pass.equals("True")){
+                            Intent i = new Intent(getBaseContext(), Main.class);
+                            if(New==true)save();
+                            startActivity(i);
+                            finish();
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                toaster("Check your username and password or your connection");
+            }
+
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+    private void toaster(String jelly){
+        Toast.makeText(this, jelly, Toast.LENGTH_SHORT).show();
+    }
+    private void check(){
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("User", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        email= pref.getString("mail", "null");
+        password= pref.getString("pass", "null");
+        New = pref.getBoolean("new",true);
+        if(email.equals("null")){
+            back=true;
+        }
+        else{
+            back=false;
+            login();
+        }
+
+    }
+    private void save(){
+        final EditText mail = findViewById(R.id.email);
+        final EditText passwd =findViewById(R.id.passwd);
+        email = mail.getText().toString();
+        password = passwd.getText().toString();
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("User", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("mail",email);
+        editor.putBoolean("new",false);
+        editor.putString("pass",password);
+        editor.commit();
     }
 }
--- app/src/main/java/com/mars/bookme/register.java
+++ app/src/main/java/com/mars/bookme/register.java
@@ -1,4 +1,163 @@
 package com.mars.bookme;
 
-public class register {
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+public class register extends AppCompatActivity {
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private String answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.register);
+        LinearLayout your_Layout = findViewById(R.id.main_container);
+        AnimationDrawable animationDrawable = (AnimationDrawable) your_Layout.getBackground();
+        animationDrawable.setEnterFadeDuration(4000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+
+    public void login(View view) {
+        post(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(getBaseContext(), logIn.class);
+        startActivity(i);
+        finish();
+    }
+
+
+    private void post(Context context) {
+        final EditText Name = findViewById(R.id.name);
+        final EditText Surname = findViewById(R.id.surname);
+        final EditText Mail = findViewById(R.id.email);
+        final EditText Passw = findViewById(R.id.passwd);
+        final EditText Passw2 = findViewById(R.id.passwd2);
+
+        if (Passw.getText().toString().equals(Passw2.getText().toString())) {
+
+            RequestQueue queue = Volley.newRequestQueue(context);
+            StringRequest sr = new StringRequest(Request.Method.POST, "http://marsdevelopers.ddns.net/", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Name", Name.getText().toString());
+                    params.put("surname", Surname.getText().toString());
+                    params.put("mail", Mail.getText().toString());
+                    params.put("pass", Passw.getText().toString());
+                    params.put("page", "register");
+
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            queue.add(sr);
+            get();
+        } else {
+            Toast.makeText(context, "ERROR, check your password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private void get (){
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://marsdevelopers.ddns.net/";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        answer = response.substring(0);
+                        if(answer.equals("Registered")){
+                            save();
+                            //toaster(answer);
+                            toaster("Registered!!");
+
+                            Intent i = new Intent(getBaseContext(), logIn.class);
+                            startActivity(i);
+                            finish();
+
+                        }
+                        else if(answer.equals("RegisterError")){
+                            toaster("It seems like this mail is used...");
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                toaster("Oops, check your connection...");
+            }
+
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+    private void toaster(String jelly){
+        Toast.makeText(this, jelly, Toast.LENGTH_SHORT).show();
+    }
+    private void save(){
+        final EditText Mail = findViewById(R.id.email);
+        final EditText Passw = findViewById(R.id.passwd);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("User", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("mail",Mail.getText().toString());
+        editor.putString("pass",Passw.getText().toString());
+        editor.commit();
+    }
 }
